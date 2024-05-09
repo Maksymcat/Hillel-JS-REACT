6b989408ddf9d5d21b9a21f730b414407298f112
Maksym <enotmaxig@gmail.com>
2024-05-09 22:40:28 +0300

    First homework

---
+++ script.js
@@ -0,0 +1,27 @@
+let number = 50;
+console.log(typeof number);
+
+let name = "Boris";
+console.log(typeof name);
+
+let homeWork = true;
+console.log(typeof homeWork);
+
+let people = {
+    name: 'Ben',
+    age: '69',
+
+}
+console.log(typeof people);
+
+let someThing = undefined;
+console.log(typeof someThing);
+
+let bigInteger = 1221512532524324324n;
+console.log(typeof bigInteger);
+
+const uniqueID = Symbol('title');
+console.log(typeof uniqueID);
+
+let bred = function(){};
+console.log(typeof bred);
