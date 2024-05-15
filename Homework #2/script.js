0d3df8dfa414452c33840bcb8021dc4e4ff7312a
Maksym <enotmaxig@gmail.com>
2024-05-15 17:06:52 +0300

    added Homework #4

--- Homework #2/script.js
+++ Homework #2/script.js
@@ -1,6 +1,6 @@
-let a = prompt('Введіть якусь цифру');
-let b = prompt('Введіть якусь цифру');
-let c = prompt('Введіть якусь цифру');
+let number = prompt('Введіть якісь цифри');
+let checkNum = 1;
+let checkNumber = 2;
 
 if(a === b && b === c && c === a){
 console.log("Всі цифри однакові")
