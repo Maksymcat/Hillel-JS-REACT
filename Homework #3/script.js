a787cd64078b85386f089789a6a2b175bdc04ecf
Maksym <enotmaxig@gmail.com>
2024-05-16 23:37:02 +0300

    Added homework#3

--- Homework #3/script.js
+++ Homework #3/script.js
@@ -1,44 +1,13 @@
-let birthYear = prompt('Вкажіть ваш рік народження');
-if(!birthYear){
-    alert("Шкода, що ти не ввів свій рік народження")
+let n = prompt("Введіть ціле число")
+
+outer : for(i = 1;i <= 100;i++){
+    let i2 = i * i;
+   
+    if(i2 > n){
+        
+    break outer;
+         
+    }
+    console.log(i)
 }
-
-let city = prompt('В якому місті ви живете?');
-if(!city){
-    alert("Шкода, що ти не ввів місто, де ти проживаєш")
-}
-
-let sport = prompt('Який ваш улюблений вид спорту?');
-if(!sport){
-    alert("Шкода, що ти не ввів свій улюблений вид спорту")
-}
-
-
-
-let yearsOld = new Date().getFullYear() - birthYear; 
-
-
-
-
-if(sport === "Бокс"){
-    alert("Круто, хочеш стати як Олександр Усик?")
-}else if(sport === "ММА"){
-    alert("Круто, хочеш стати як Конор МакГрегор?")
-}else if(sport === "Біг"){
-    alert("Круто, хочеш стати як Усейн Болт?")
-}
-
-if(city === "Київ"){
-    alert(`Тобі ${yearsOld} років ,Ти живеш у столиці України, ${city}`);
-}
- else if(city === "Вашингтон"){
-    alert(`Тобі ${yearsOld} років, Ти живеш у столиці США, ${city}`);
-}
- else if(city === "Лондон"){
-    alert(`Тобі ${yearsOld} років, Ти живеш у столиці Англії, ${city}`);
-}
-if(birthYear,city,sport) {
-    alert(`Тобі ${yearsOld} років, Ти живеш у місті ${city} ` );
-}
-
 
