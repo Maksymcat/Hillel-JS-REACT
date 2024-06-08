1790d87ade127b017170e6b11a2e9cc0553ffa1d
Maksym <enotmaxig@gmail.com>
2024-06-08 21:45:42 +0300

    new Homework

--- Homework #1/script.js
+++ Homework #1/script.js
@@ -1,13 +1,16 @@
-let  usName;
-let salary;
-let someObject = {
-name: 'mariHanna',
-salary: 15000,
-myFunc: function bebebe(...someBase){
-    usName = this.name
-    salary = this.salary
-    console.log('Імя === ' + `${usName}`+ ' Зарплата === ' + `${salary}`)
-    return;
-}
-}
-console.log(someObject.myFunc())
+let sumOfSalary = 0;
+let i = 0
+let grogling;
+let company = {
+    sales: [{name: 'John', salary: 15000}, {name: 'Alice', salary:10000},{name: 'baby',age: 15, salary: 15000}],
+    brother: "just bro",}
+
+        function findByUserName(userNickName){
+          const result = company.sales.filter((user) => {
+            
+return user.salary 
+          })
+          return result;
+        }
+console.log
+console.log(findByUserName('Alice'))
--- Homework #2/script.js
+++ Homework #2/script.js
@@ -1,11 +1,28 @@
-function bebebe(){
-    let sum = 0
-return function(value){
-    sum += value
-    return sum;
-};
-   
+
+let someObject = {
+phoneBook: [{name: 'Maksym',
+phoneNumber: '+38099321543',
+email: 'enotglobaldisfunction@gmail.com',},
+ {name:'Vanya',phoneNumber:'+38023213535',email:'vanyoknehacker@gmail.com',},
+ {name:'Oleksii',phoneNumber:'+3807777777',email:'lyohazeroproblem@gmail.com',}],
+findUserByName(userNickName){
+let result = this.phoneBook.filter((user) => {
+    return user.name === userNickName
+})
+return result;
+},
+
+
+
+
 }
-let letting = bebebe()
-console.log(letting(10))
-console.log(letting(20))
+function CreateNewContact(name,phoneNumber,email){
+    this.name = name 
+    this.phoneNumber = phoneNumber
+    this.email = email
+  
+    }
+
+let newUser = new CreateNewContact('aleksandr','38093242344','sashaverni100@gmail.com')
+someObject.phoneBook.push(newUser)
+console.log(someObject.findUserByName('aleksandr'))
