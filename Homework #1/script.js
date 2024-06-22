07e932ba4a6c304eab25b8dbf455ab3a3df7c6b7
Maksym <enotmaxig@gmail.com>
2024-06-22 23:57:39 +0300

    new DZ

--- Homework #1/script.js
+++ Homework #1/script.js
@@ -1,30 +1,20 @@
-function printMultiplicationTable(n) {
-  // Перевірка, що n - це ціле число і більше 1
-  if (!Number.isInteger(n) || n < 1) {
-      console.log('Введіть ціле число більше 0');
-      return;
-  }
-  
-  // Виведення заголовка таблиці
-  console.log(`Таблиця Піфагора для чисел від 1 до ${n}:`);
-  
-  // Виведення заголовку першого рядка (заголовки стовпців)
-  let headerRow = '   ';
-  for (let i = 1; i <= n; i++) {
-      headerRow += `${i.toString().padEnd(4)}`;
-  }
-  console.log(headerRow);
-  
-  // Виведення таблиці
-  for (let i = 1; i <= n; i++) {
-      let row = `${i.toString().padEnd(2)} `;
-      for (let j = 1; j <= n; j++) {
-          row += `${(i * j).toString().padEnd(4)}`;
-      }
-      console.log(row);
-  }
-}
+let container = document.querySelector(".container")
 
-// Приклад виклику функції для виведення таблиці Піфагора для чисел від 1 до 10
-printMultiplicationTable(10);
 
+
+container.addEventListener('click', function(event) {
+    let target = event.target 
+    console.log(event)
+    if(target.tagName == 'BUTTON'){
+        if(target.className == 'btn-1'){
+            confirm("Ви натиснули кнопку 1 " )
+        } else
+        if(target.className == 'btn-2'){
+           confirm("Ви натиснули кнопку 2")
+        }else 
+       
+            confirm("Ви натиснули кнопку 3")
+        
+           }
+           
+})
