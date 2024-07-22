fbf9896d57980c5f288b69d2b8ff5cfd19a1c067
Maksym <enotmaxig@gmail.com>
2024-07-22 15:44:55 +0300

    fetch training

--- Homework #3/script.js
+++ Homework #3/script.js
@@ -1,5 +1,33 @@
-let apiUrl = `https://openweathermap.org/current/weather.id`
-fetch(apiUrl)
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.log('pomilka', error))
+let apiKey = '5151b416bdebc3f70c745987946a52fa'
+let div = document.getElementById('bob')
+let diver = document.getElementById('bober')
+let refreshWeather = document.getElementById('refreshWeatherjs')
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  getWeatherData()
+   useWeatherData()
+ 
+  })
+  
+
+
+
+    function getWeatherData(){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kuznetsovsk&lang=ru&appid=${apiKey}&units=metric`)
+  .then(response => response.json())
+  .then(function(json){
+   useWeatherData(json)
+ 
+  })
+    }
+refreshWeather.addEventListener('click',function(){
+setTimeout(getWeatherData, 1000);
+  
+})
+
+function useWeatherData(json){
+    div.textContent = `humidity = ${json.main.humidity}%,  temp = ${json.main.temp}, feels like =  ${json.main.feels_like}`
+   diver.textContent = `Хмарність = ${json.weather[0].description} Швидкість ${json.wind.speed}km/h, pressure = ${json.main.pressure}`
+}
