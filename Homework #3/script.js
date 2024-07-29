8ad07815913dd75eb8b098bb273864d782b698eb
Maksym <enotmaxig@gmail.com>
2024-07-29 15:54:21 +0300

    FOR IN cicles,recursion, somehard

--- Homework #3/script.js
+++ Homework #3/script.js
@@ -1,33 +1,38 @@
 let company = {
   sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
   development: {
-web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
+web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800},{name: 'ben', salary: 2000}],
 internals: [{name: 'Jack', salary: 1300}]
   }
-  
 }
-let couter;
-let counter;
-function sumOfSalary(obj){
-  couter = 0
-  counter = 0
-    for(key in obj){
-      let salaryDI = obj.development.internals[counter].salary
-      let salaryDW = obj.development.web[counter].salary
-      let salarySales = obj.sales[counter].salary
-      counter++
-      couter += salaryDI
-      couter += salarySales
-      couter += salaryDW
-   
-      console.log(couter)
-     if(counter >= 2){
-      console.log('worked')
-      counter = 0 
-      return sumOfSalary(obj.development.web[counter].salary)
-     }
-  }
-  return sumOfSalary(obj.sales);
-}
-sumOfSalary(company)
 
+let sumOfSalarys = 0
+
+function sumOfSalary(obj){
+  
+  for(key in obj){
+   
+      let chooseObj = obj[key]
+if(Array.isArray(chooseObj)){
+  for(key2 of chooseObj){
+
+    sumOfSalarys += key2.salary
+    
+  }
+}  else{
+  return sumOfSalary(chooseObj)
+
+}
+
+
+    }
+
+
+  
+    
+  
+  console.log(sumOfSalarys)
+}
+
+
+sumOfSalary(company)
