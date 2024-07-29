e7c1de84993207aa401b29872c7953b5bac62f17
Maksym <enotmaxig@gmail.com>
2024-07-29 15:55:07 +0300

    FOR IN, cicles, someHard)

--- Homework #3/script.js
+++ Homework #3/script.js
@@ -1,38 +1,33 @@
 let company = {
-  sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
+  sales: [
+    { name: "John", salary: 1000 },
+    { name: "Alice", salary: 600 },
+  ],
   development: {
-web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800},{name: 'ben', salary: 2000}],
-internals: [{name: 'Jack', salary: 1300}]
-  }
-}
+    web: [
+      { name: "Peter", salary: 2000 },
+      { name: "Alex", salary: 1800 },
+      { name: "ben", salary: 2000 },
+    ],
+    internals: [{ name: "Jack", salary: 1300 }],
+  },
+};
 
-let sumOfSalarys = 0
+let sumOfSalarys = 0;
 
-function sumOfSalary(obj){
-  
-  for(key in obj){
-   
-      let chooseObj = obj[key]
-if(Array.isArray(chooseObj)){
-  for(key2 of chooseObj){
-
-    sumOfSalarys += key2.salary
-    
-  }
-}  else{
-  return sumOfSalary(chooseObj)
-
-}
-
-
+function sumOfSalary(obj) {
+  for (key in obj) {
+    let chooseObj = obj[key];
+    if (Array.isArray(chooseObj)) {
+      for (key2 of chooseObj) {
+        sumOfSalarys += key2.salary;
+      }
+    } else {
+      return sumOfSalary(chooseObj);
     }
+  }
 
-
-  
-    
-  
-  console.log(sumOfSalarys)
+  console.log(sumOfSalarys);
 }
 
-
-sumOfSalary(company)
+sumOfSalary(company);
