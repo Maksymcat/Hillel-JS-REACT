c3b791373e4abc2e96f7867ea1c2e7711d483047
Maksym <enotmaxig@gmail.com>
2024-08-01 10:31:24 +0300

    WebStorageTry1

--- Homework #3/script.js
+++ Homework #3/script.js
@@ -1,22 +1,23 @@
-const createWork = document.querySelector(".createWork");
-const toDoList = document.querySelector(".to-do-list");
+const createWork = document.querySelector(".form__btn");
+const toDoList = document.querySelector(".js--todos-wrapper");
 
-const dataInput = document.querySelector(".textOfWork");
+const dataInput = document.querySelector(".js--form__input");
 
 
 
 
 createWork.addEventListener("click", function (event) {
    let li = document.createElement("li");
-  li.textContent = dataInput.value;
+
+  li.classList.add('todo-item')
   toDoList.appendChild(li);
   let buttonForDelete = document.createElement("button");
   li.appendChild(buttonForDelete);
-  buttonForDelete.classList.add("btn-delete");
+  buttonForDelete.classList.add("todo-item__delete");
   buttonForDelete.textContent = "Видалити завдання";
 
   li.addEventListener("click", (event) => {
-    if (event.target.classList.contains("btn-delete")) {
+    if (event.target.classList.contains("todo-item__delete")) {
       toDoList.removeChild(li);
     }
   });
