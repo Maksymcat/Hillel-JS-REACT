32f5d845d771297c2469bd679298aca283e8f589
Maksym <enotmaxig@gmail.com>
2024-08-16 22:06:28 +0300

    jQuery & Botstrap someTrain LS20

--- ToDos/main.js
+++ ToDos/main.js
@@ -1,50 +1,40 @@
-
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 let renderToDos;
-document.addEventListener("DOMContentLoaded", function () {
-  const ul = document.querySelector(".js--todos-wrapper");
 
-  const saveToDoBtn = document.querySelector(".form__btn");
+const $youQuest = $(".modal-title");
+const $textOfTodo = $(".modal-body");
+$(document).ready(function () {
+  const $ul = $(".js--todos-wrapper");
+  const $saveToDoBtn = $(".form__btn");
 
   let currentUserIndex = null;
 
   renderToDos = () => {
-  
-    ul.innerHTML = "";
+    $ul.empty();
 
     todos.forEach((todo, index) => {
-      const li = document.createElement("li");
-      li.classList.add("todo-item");
-      let todosData = localStorage.getItem("todos");
-   
-      li.innerHTML = `
-      <p class="naInputNeVeshaetsya"   onclick="checked(${index})">  <input type="checkbox" ></p>
-        <span class="todo-item__description">${todo.name}</span>
-                 <button class="todo-item__delete" onclick="removeToDo(${index})">Видалити</button>
-         `;
-         if (JSON.parse(todosData)[index].checked == true) {
-          li.innerHTML = `
-          <p class="naInputNeVeshaetsya"   onclick="checked(${index})">  <input type="checkbox" checked></p>
-            <span class="todo-item__description">${todo.name}</span>
-                     <button class="todo-item__delete" onclick="removeToDo(${index})">Видалити</button>
-             `;
-         }
-       
+      const todosData = localStorage.getItem("todos");
+      const isChecked = JSON.parse(todosData)[index].checked;
 
-      ul.appendChild(li);
-      
+      const $li = $("<li>").addClass("todo-item");
 
-  
+      $li.attr('data-bs-toggle="modal" data-bs-target="#exampleModal');
+      $li.html(`
+        <p class="naInputNeVeshaetsya" onclick="checked(${index})">
+          <input type="checkbox" ${isChecked ? "checked" : ""}>
+        </p>
+        <span data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showTheQuest(${index})" class="todo-item__description">${
+        todo.name
+      }</span>
+        <button class="todo-item__delete" onclick="removeToDo(${index})">Видалити</button>
+      `);
+
+      $ul.append($li);
     });
- 
- 
   };
-  
 
- 
-
-  saveToDoBtn.addEventListener("click", () => {
-    const name = document.querySelector(".js--form__input").value;
+  $saveToDoBtn.on("click", () => {
+    const name = $(".js--form__input").val();
 
     if (currentUserIndex === null) {
       const id = todos.length ? todos[todos.length - 1].id + 1 : 1;
@@ -58,10 +48,12 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   renderToDos();
-
-  
 });
 
+let showTheQuest = (index) => {
+  $youQuest.html(`Ваше завдання`);
+  $textOfTodo.html(`${todos[index].name}`);
+};
 let removeToDo = (index) => {
   if (confirm("Are you sure you want to delete this todo?")) {
     todos.splice(index, 1);
@@ -69,11 +61,12 @@ let removeToDo = (index) => {
     renderToDos();
   }
 };
-let   checked = (index) => {
-let completed = {checked:true }
+
+let checked = (index) => {
+  let completed = { checked: true };
   if (todos[index].checked == true || null) {
-     completed = {checked: false }
-  } 
-  Object.assign(todos[index],completed)
+    completed = { checked: false };
+  }
+  Object.assign(todos[index], completed);
   localStorage.setItem("todos", JSON.stringify(todos));
 };
